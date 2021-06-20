430972a3e90cb8e43a559f8fd825728ad28c8c8b
shyne <shyne9795@gmail.com>
2021-06-20 11:07:52 +0900

    learn 17-2

---
+++ 17/2/gui/src/main.rs
@@ -0,0 +1,44 @@
+extern crate gui;
+use gui::Draw;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        //セレクトボックスを実際に描画するコード
+    }
+}
+
+use gui::{Screen, Button};
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    // はい
+                    String::from("Yes"),
+                    // 多分
+                    String::from("Maybe"),
+                    // いいえ
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                // 了解
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
